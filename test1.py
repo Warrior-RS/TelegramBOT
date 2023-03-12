adc1a0f60f32c591470fd337f46e9e0949e3b8c9
IvanI <warriorrs@gmail.com>
2023-03-12 15:06:48 +0100

    UpDate

--- test1.py
+++
@@ -1,9 +0,0 @@
-from rembg import remove
-from PIL import Image
-
-input_path = 'C:/news/2022-10-22.png'
-output_path = 'C:/news/2022-10-22up.png'
-
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
